61df5670564f298d510c5db9350b57f126e72e06
dratum <devoonwtf@gmail.com>
2025-08-26 23:09:35 +0300

    Добавил репозиторий

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -5,18 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 
-	desc "github.com/dratum/auth/pkg/auth_v1"
+	"github.com/dratum/auth/internal/domain/models"
+	"github.com/dratum/auth/pkg/auth_v1"
 	"github.com/jackc/pgx/v4"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 const grpcPort = 50051
 const dbDSN = ""
 
 type server struct {
-	desc.UnimplementedAuthV1Server
+	auth_v1.UnimplementedAuthV1Server
+	db *pgx.Conn
 }
 
 func ConnectDB(ctx context.Context, dbDSN string) (*pgx.Conn, error) {
@@ -25,6 +29,54 @@ func ConnectDB(ctx context.Context, dbDSN string) (*pgx.Conn, error) {
 		return nil, err
 	}
 	return conn, nil
+}
+
+func (s *server) Get(
+	ctx context.Context,
+	req *auth_v1.GetRequest) (*auth_v1.GetResponse, error) {
+	query := `
+			  select id
+			       , name
+			       , email
+			       , role
+			       , created_at
+			       , updated_at
+			  from users u
+			  where u.id = $1
+			`
+
+	var user models.User
+	var roleStr string
+
+	err := s.db.QueryRow(ctx, query, req.Id).Scan(
+		&user.Id,
+		&user.Email,
+		&user.Name,
+		&roleStr,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %w", err)
+	}
+
+	roleValue, exists := auth_v1.Role_value[strings.ToUpper(roleStr)]
+	if !exists {
+		return nil, fmt.Errorf("invalid role value from database: %s", roleStr)
+	}
+
+	return &auth_v1.GetResponse{
+		Id:        user.Id,
+		Email:     user.Email,
+		Name:      user.Name,
+		Role:      auth_v1.Role(roleValue),
+		CreatedAt: timestamppb.New(user.CreatedAt),
+		UpdatedAt: timestamppb.New(user.UpdatedAt),
+	}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *auth_v1.CreateRequest) (*auth_v1.CreateResponse, error) {
+	return &auth_v1.CreateResponse{}, nil
 }
 
 func main() {
@@ -43,7 +95,7 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterAuthV1Server(s, &server{})
+	auth_v1.RegisterAuthV1Server(s, &server{})
 
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
---
+++ internal/repository/repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/dratum/auth/pkg/auth_v1"
+)
+
+type UserRepository interface {
+	Get(ctx context.Context, id int64) (*auth_v1.GetResponse, error)
+	Create(ctx context.Context, fields *auth_v1.CreateRequest) (int64, error)
+	Update(ctx context.Context, id int64) error
+	Delete(ctx context.Context, id int64) error
+}
